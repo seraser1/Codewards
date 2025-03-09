ae1152e3a3cc79158d39f969f978720fc11745b7
Nikita <seraser90@gmail.com>
2025-03-10 02:48:19 +0300

    perimeter sequence

---
+++ 7-kyi/Perimeter_sequence.js
@@ -0,0 +1,9 @@
+// The first three stages of a sequence are shown.
+
+// blocks
+
+// The blocksize is a by a and a ≥ 1.
+
+// What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+
+const perimeterSequence = (a,n) => 4 * n * a
