6356bfa4de3e09abe3e39b74cf6f85a6ee2cdba2
Nikita <seraser90@gmail.com>
2025-04-07 14:01:30 +0300

    A Promise is a Promise

---
+++ 7-kyi/A_Promise_is_a_Promise.js
@@ -0,0 +1,12 @@
+// Create a resolved javascript Promise that will return 'Hello World!'.
+
+function promiseHelloWorld() {
+    // replace this nonsense with your Promise
+    return Promise.resolve('Hello World!');
+}
+
+// const promiseHelloWorld = new Promise((resolve, reject) =>{
+//     setTimeout(() => resolve('Hello World!'), 1000);
+// }).then((message) => {
+//     console.log(message);
+// });
